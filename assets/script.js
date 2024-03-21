336d058d0840072a762fed76b0878f3a83181b0a
unknown <agnesgeller@cyberanalys.com>
2024-03-21 22:36:04 +0000

    footer menu added

--- assets/script.js
+++ assets/script.js
@@ -62,3 +62,37 @@ let words = document.querySelectorAll(".word");
 
     // mix it up portfolio section
     var mixer = mixitup('.portfolio-gallery');
+
+
+
+      // active menu ///
+      let menuLi = document.querySelectorAll('header ul li a');
+      let section = document.querySelectorAll('section');
+
+      function activeMenu(){
+        let len = section.length;
+        while(--len && window.scrollY + 97 < section[len -1].offsetTop){}
+        menuLi.forEach(sec => sec.classList.remove("active"));
+        menuLi[len].classList.add("active");
+      }
+
+      activeMenu();
+      window.addEventListener("scroll",activeMenu);
+
+
+
+      // sticky navbar ///
+      const header = document.querySelector("header");
+      window.addEventListener("scroll",function(){
+        header.classList.toggle("sticky",window.scrollY > 50)
+      })
+      
+
+       // toggle icon navbar ///
+       let menuIcon = document.querySelector("#menu-icon");
+       let navlist = document.querySelector(".navlist");
+
+       menuIcon.onclick = ()=>{
+        menuIcon.classList.toggle("bx-x");
+         navlist.classList.toggle("open");
+       }
